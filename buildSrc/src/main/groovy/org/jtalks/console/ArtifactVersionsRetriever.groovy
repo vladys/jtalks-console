b88baa14328a5745dcf64d02f767163bde8f6b9d
ctapobep <ctapobep@javatalks.ru>
2012-08-19 23:10:20 +0200

    Added ability to show both JCommune and Poulpe released versions

--- buildSrc/src/main/groovy/org/jtalks/console/ArtifactVersionsRetriever.groovy
+++ buildSrc/src/main/groovy/org/jtalks/console/ArtifactVersionsRetriever.groovy
@@ -3,22 +3,27 @@ package org.jtalks.console
  * @author stanislav bashkirtsev
  */
 class ArtifactVersionsRetriever {
-    private final static String POULPE_RELEASES_PAGE = "http://repo.jtalks.org/content/repositories/releases/org/jtalks/poulpe/poulpe-web-view/"
+  private final static String POULPE_RELEASES_PAGE = "http://repo.jtalks.org/content/repositories/releases/org/jtalks/poulpe/poulpe-web-view/"
+  private final static String JCOMMUNE_RELEASES_PAGE = "http://repo.jtalks.org/content/repositories/releases/org/jtalks/jcommune/jcommune-web-view/"
 
-    public List<String> getPoulpeReleaseVersions(){
-        return getVersions(POULPE_RELEASES_PAGE)
-    }
+  public String getPoulpeReleaseVersionsOutput() {
+    return "Poulpe released versions: " + getVersions(POULPE_RELEASES_PAGE) + "\n"
+  }
 
-    private List<String> getVersions(String urlWithVersions) {
-        List<String> versions = []
-        String pageContent = new URL(urlWithVersions).openStream().getText()
-        String[] refsFromPage = pageContent.findAll(/>(.+)<\/a/)
-        for (String s : refsFromPage) {
-            String versionName = s.replaceAll(">", "").replaceAll("</a", "")
-            if (versionName.contains("/")) {
-                versions.add(versionName.replace("/", ""))
-            }
-        }
-        versions
+  public String getJcommuneReleaseVersionsOutput() {
+    return "JCommune released versions: " + getVersions(JCOMMUNE_RELEASES_PAGE) + "\n"
+  }
+
+  private List<String> getVersions(String urlWithVersions) {
+    List<String> versions = []
+    String pageContent = new URL(urlWithVersions).openStream().getText()
+    String[] refsFromPage = pageContent.findAll(/>(.+)<\/a/)
+    for (String s : refsFromPage) {
+      String versionName = s.replaceAll(">", "").replaceAll("</a", "")
+      if (versionName.contains("/")) {
+        versions.add(versionName.replace("/", ""))
+      }
     }
+    versions
+  }
 }
