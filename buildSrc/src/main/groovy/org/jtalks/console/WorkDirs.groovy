8cdf56e8822f5be2e4b87b9d4938f6d7af416edc
ctapobep <ctapobep@javatalks.ru>
2012-08-20 12:59:57 +0200

    Implemented unzip functionality for the web server

--- buildSrc/src/main/groovy/org/jtalks/console/WorkDirs.groovy
+++ buildSrc/src/main/groovy/org/jtalks/console/WorkDirs.groovy
@@ -4,7 +4,8 @@ package org.jtalks.console
  * @author stanislav bashkirtsev
  */
 class WorkDirs {
-  private final String[] dirs = ["./workdir/apps", "./workdir/servers", "./workdir/configs", "./workdir/deployed"]
+  private final String[] dirs = ["./workdir/apps/", "./workdir/servers/", "./workdir/configs/", "./workdir/deployed/"]
+  private final def config = new ConfigSlurper().parse(new File("gradle.properties").toURL())
   final downloadedAppsDir = dirs[0], wevServersDir = dirs[1], configsDir = dirs[2], deployedAppsDir = dirs[3]
 
   void createDirs() {
@@ -24,17 +25,28 @@ class WorkDirs {
   }
 
   void downloadJtalksApp(String address) {
-    def file = new FileOutputStream(downloadedAppsDir + "/" + address.tokenize("/")[-1])
+    def file = new FileOutputStream(downloadedAppsDir + address.tokenize("/")[-1])
     def out = new BufferedOutputStream(file)
     out << new URL(address).openStream()
     out.close()
   }
 
-  void downloadTomcat(String tomcatFileName) {
-    String address = "http://ftp.ps.pl/pub/apache/tomcat/tomcat-6/v6.0.35/bin/$tomcatFileName" + ".zip"
-    def file = new FileOutputStream(wevServersDir + "/" + address.tokenize("/")[-1])
+  void unpackServer() {
+
+//    def zipFile = new java.util.zip.ZipFile(new File(wevServersDir + config.server.name + ".zip"))
+  }
+
+  void downloadTomcat() {
+    String address = "http://ftp.ps.pl/pub/apache/tomcat/tomcat-6/v6.0.35/bin/" + config.server.name + ".zip"
+    def file = new FileOutputStream(wevServersDir + address.tokenize("/")[-1])
     def out = new BufferedOutputStream(file)
     out << new URL(address).openStream()
     out.close()
+
+    def ant = new AntBuilder()   // create an antbuilder
+
+    ant.unzip(  src: wevServersDir + config.server.name + ".zip",
+        dest: wevServersDir,
+        overwrite:"false" )
   }
 }
